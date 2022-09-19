cd2f22e11b42489f1e9cf2bb59d46987ae5b49eb
Ashley Wulber <ashley@system76.com>
2022-09-19 11:01:02 -0400

    first commit

---
+++ cosmic-bg-config/src/lib.rs
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MPL-2.0-only
+
+use std::{path::{PathBuf}, fs::File};
+
+use serde::{Deserialize, Serialize};
+use xdg::BaseDirectories;
+
+/// Configuration for the panel's ouput
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
+#[serde(deny_unknown_fields)]
+pub enum CosmicBgOuput {
+    /// show panel on all outputs
+    All,
+    /// show panel on a specific output
+    MakeModel {
+        make: String,
+        model: String,
+    },
+}
+
+/// Configuration for the panel's ouput
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
+#[serde(deny_unknown_fields)]
+pub enum CosmicBgImgSource {
+    /// pull images from the $HOME/Pictures/Wallpapers directory
+    Wallpapers,
+    /// pull images from a specific directory or file
+    Path(String),
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct CosmicBgEntry {
+    /// the configured output
+    pub output: CosmicBgOuput,
+    /// the configured image source
+    pub source: CosmicBgImgSource,
+    /// whether the images should be filtered by the active theme
+    pub filter_by_theme: bool,
+    /// frequency at which the wallpaper is rotated in minutes
+    pub rotation_frequency: u32,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct CosmicBgConfig {
+    /// the configured wallpapers
+    pub backgrounds: Vec<CosmicBgEntry>
+}
+
+impl Default for CosmicBgConfig {
+    fn default() -> Self {
+        CosmicBgConfig {
+            backgrounds: vec![CosmicBgEntry {
+                output: CosmicBgOuput::All,
+                source: CosmicBgImgSource::Wallpapers,
+                filter_by_theme: true,
+                rotation_frequency: 60,
+            }]
+        }
+    }
+}
+
+static NAME: &str = "com.system76.CosmicBg";
+static CONFIG: &str = "config.ron";
+
+
+impl CosmicBgConfig {
+    /// load config with the provided name
+    pub fn load() -> anyhow::Result<Self> {
+        let config_path: PathBuf = vec![NAME, CONFIG].iter().collect();
+        let config_path =
+            match BaseDirectories::new().map(|dirs| dirs.find_config_file(&config_path)) {
+                Ok(Some(path)) => path,
+                _ => anyhow::bail!("Failed to get find config file"),
+            };
+
+        let file = match File::open(&config_path) {
+            Ok(file) => file,
+            Err(err) => {
+                anyhow::bail!("Failed to open '{}': {}", config_path.display(), err);
+            }
+        };
+
+        match ron::de::from_reader::<_, Self>(file) {
+            Ok(config) => Ok(config),
+            Err(err) => {
+                anyhow::bail!("Failed to parse '{}': {}", config_path.display(), err);
+            }
+        }
+    }
+
+    /// write config to config file
+    pub fn write(&self) -> anyhow::Result<()> {
+        let config_path: PathBuf = vec![NAME, CONFIG].iter().collect();
+        let xdg = BaseDirectories::new()?;
+        let f = xdg.place_config_file(&config_path).unwrap();
+        let f = File::create(f)?;
+        ron::ser::to_writer_pretty(&f, self, ron::ser::PrettyConfig::default())?;
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,377 @@
+// SPDX-License-Identifier: MPL-2.0-only
+
+use std::{collections::VecDeque, convert::TryInto, path::PathBuf, time::Duration};
+
+use cosmic_bg_config::{CosmicBgConfig, CosmicBgOuput};
+use image::{io::Reader as ImageReader, DynamicImage};
+use sctk::{
+    compositor::{CompositorHandler, CompositorState},
+    delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_shm,
+    output::{OutputHandler, OutputInfo, OutputState},
+    reexports::{
+        calloop,
+        client::{
+            protocol::{
+                wl_output::{self, WlOutput},
+                wl_shm, wl_surface,
+            },
+            Connection, EventQueue, QueueHandle,
+        },
+    },
+    registry::{ProvidesRegistryState, RegistryState},
+    registry_handlers,
+    shell::layer::{
+        KeyboardInteractivity, Layer, LayerHandler, LayerState, LayerSurface, LayerSurfaceConfigure,
+    },
+    shm::{slot::SlotPool, ShmHandler, ShmState},
+};
+
+fn main() -> anyhow::Result<()> {
+    let conn = Connection::connect_to_env().unwrap();
+
+    let mut event_loop = calloop::EventLoop::try_new()?;
+    let event_queue: EventQueue<CosmicBg> = conn.new_event_queue();
+    let qh = event_queue.handle();
+    let mut config = CosmicBgConfig::load().unwrap_or_default();
+
+    // initial setup with all imagesf
+    let wallpapers = config
+        .backgrounds
+        .iter()
+        .map(|bg| {
+            let mut image_queue = VecDeque::new();
+            let cur_image = image_queue.pop_front().and_then(|cur_image_path| {
+                let img = match ImageReader::open(&cur_image_path) {
+                    Ok(img) => match img.decode() {
+                        Ok(img) => Some(img),
+                        Err(_) => return None,
+                    },
+                    Err(_) => return None,
+                };
+                image_queue.push_back(cur_image_path);
+                img
+            });
+            CosmicBgWallpaper {
+                configured_output: bg.output.clone(),
+                layers: Vec::new(),
+                cur_image,
+                image_queue,
+                filter_by_theme: bg.filter_by_theme,
+                rotation_frequency: bg.rotation_frequency,
+                pool: None,
+            }
+        })
+        .collect();
+
+    // XXX All entry if it exists, should be placed last in the list of wallpapers
+    let mut bg_state = CosmicBg {
+        registry_state: RegistryState::new(&conn, &qh),
+        output_state: OutputState::new(),
+        compositor_state: CompositorState::new(),
+        shm_state: ShmState::new(),
+        layer_state: LayerState::new(),
+
+        exit: false,
+        wallpapers,
+        config,
+    };
+
+    while !bg_state.registry_state.ready() {
+        event_loop
+            .dispatch(Duration::from_millis(16), &mut bg_state)
+            .unwrap();
+    }
+
+    loop {
+        event_loop.dispatch(Duration::from_millis(16), &mut bg_state)?;
+
+        if bg_state.exit {
+            println!("exiting example");
+            break;
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+struct CosmicBgWallpaper {
+    configured_output: CosmicBgOuput,
+    layers: Vec<CosmicBgLayer>,
+    cur_image: Option<DynamicImage>,
+    image_queue: VecDeque<PathBuf>,
+    filter_by_theme: bool,
+    rotation_frequency: u32,
+    pool: Option<SlotPool>,
+}
+
+#[derive(Debug)]
+struct CosmicBgLayer {
+    layer: LayerSurface,
+    wl_output: WlOutput,
+    output_info: OutputInfo,
+    first_configure: bool,
+    width: u32,
+    height: u32,
+}
+
+#[derive(Debug)]
+struct CosmicBg {
+    registry_state: RegistryState,
+    output_state: OutputState,
+    compositor_state: CompositorState,
+    shm_state: ShmState,
+    layer_state: LayerState,
+
+    exit: bool,
+    wallpapers: Vec<CosmicBgWallpaper>,
+    config: CosmicBgConfig,
+}
+
+impl CompositorHandler for CosmicBg {
+    fn compositor_state(&mut self) -> &mut CompositorState {
+        &mut self.compositor_state
+    }
+
+    fn scale_factor_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _new_factor: i32,
+    ) {
+        // Not needed for this example.
+    }
+
+    fn frame(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _time: u32,
+    ) {
+        for wallpaper in &mut self.wallpapers {
+            wallpaper.draw(qh);
+        }
+    }
+}
+
+impl OutputHandler for CosmicBg {
+    fn output_state(&mut self) -> &mut OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+        let output_info = match self.output_state.info(&output) {
+            Some(info) => info,
+            None => return,
+        };
+
+        let item = match self
+            .wallpapers
+            .iter_mut()
+            .find(|w| match &w.configured_output {
+                CosmicBgOuput::All => true,
+                CosmicBgOuput::MakeModel { make, model } => {
+                    make == &output_info.make && model == &output_info.model
+                }
+            }) {
+            Some(item) => item,
+            None => return,
+        };
+
+        let (width, height) = match item.cur_image.as_ref() {
+            Some(img) => (
+                img.width().min(output_info.physical_size.0 as u32),
+                img.height().min(output_info.physical_size.1 as u32),
+            ),
+            None => (1, 1),
+        };
+
+        if item.pool.is_none() {
+            let pool = SlotPool::new(width as usize * height as usize * 4, &self.shm_state)
+                .expect("Failed to create pool");
+            item.pool.replace(pool);
+        }
+
+        let surface = self.compositor_state.create_surface(&qh).unwrap();
+
+        let layer = LayerSurface::builder()
+            .size((1, 1))
+            .keyboard_interactivity(KeyboardInteractivity::None)
+            .namespace("wallpaper")
+            .map(&qh, &mut self.layer_state, surface, Layer::Background)
+            .expect("layer surface creation");
+
+        item.layers.push(CosmicBgLayer {
+            layer,
+            wl_output: output,
+            output_info,
+            width,
+            height,
+            first_configure: false,
+        });
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+        // TODO
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+        let output_info = match self.output_state.info(&output) {
+            Some(info) => info,
+            None => return,
+        };
+
+        let item = match self
+            .wallpapers
+            .iter_mut()
+            .find(|w| match &w.configured_output {
+                CosmicBgOuput::All => true,
+                CosmicBgOuput::MakeModel { make, model } => {
+                    make == &output_info.make && model == &output_info.model
+                }
+            }) {
+            Some(item) => item,
+            None => return,
+        };
+
+        let layer_position = match item
+            .layers
+            .iter()
+            .position(|bg_layer| bg_layer.wl_output == output)
+        {
+            Some(layer) => layer,
+            None => return,
+        };
+        item.layers.remove(layer_position);
+    }
+}
+
+impl LayerHandler for CosmicBg {
+    fn layer_state(&mut self) -> &mut LayerState {
+        &mut self.layer_state
+    }
+
+    fn closed(&mut self, _conn: &Connection, _qh: &QueueHandle<Self>, _layer: &LayerSurface) {
+        self.exit = true;
+    }
+
+    fn configure(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        layer: &LayerSurface,
+        configure: LayerSurfaceConfigure,
+        _serial: u32,
+    ) {
+        for wallpaper in &mut self.wallpapers {
+            let mut draw = false;
+            for w_layer in &mut wallpaper.layers {
+                if &w_layer.layer == layer && configure.new_size.0 != 0 && configure.new_size.1 != 0
+                {
+                    w_layer.width = configure.new_size.0;
+                    w_layer.height = configure.new_size.1;
+                    draw = true;
+                    if w_layer.first_configure {
+                        w_layer.first_configure = false;
+                    }
+                }
+            }
+            if draw {
+                wallpaper.draw(qh);
+            }
+        }
+    }
+}
+
+impl ShmHandler for CosmicBg {
+    fn shm_state(&mut self) -> &mut ShmState {
+        &mut self.shm_state
+    }
+}
+
+impl CosmicBgWallpaper {
+    pub fn draw(&mut self, qh: &QueueHandle<CosmicBg>) {
+        for layer in &self.layers {
+            let width = layer.width;
+            let height = layer.height;
+            let stride = layer.width as i32 * 4;
+            let pool = self.pool.as_mut().unwrap();
+
+            let (buffer, canvas) = pool
+                .create_buffer(
+                    width as i32,
+                    height as i32,
+                    stride,
+                    wl_shm::Format::Argb8888,
+                )
+                .expect("create buffer");
+
+            // Draw to the window:
+            {
+                canvas
+                    .chunks_exact_mut(4)
+                    .enumerate()
+                    .for_each(|(index, chunk)| {
+                        let x = (index % width as usize) as u32;
+                        let y = (index / width as usize) as u32;
+
+                        let a = 0xFF;
+                        let r =
+                            u32::min(((width - x) * 0xFF) / width, ((height - y) * 0xFF) / height);
+                        let g = u32::min((x * 0xFF) / width, ((height - y) * 0xFF) / height);
+                        let b = u32::min(((width - x) * 0xFF) / width, (y * 0xFF) / height);
+                        let color = (a << 24) + (r << 16) + (g << 8) + b;
+
+                        let array: &mut [u8; 4] = chunk.try_into().unwrap();
+                        *array = color.to_le_bytes();
+                    });
+            }
+
+            let wl_surface = layer.layer.wl_surface();
+            // Damage the entire window
+            wl_surface.damage_buffer(0, 0, width as i32, height as i32);
+
+            // Request our next frame
+            layer.layer.wl_surface().frame(qh, wl_surface.clone());
+
+            // Attach and commit to present.
+            buffer.attach_to(&wl_surface).expect("buffer attach");
+            wl_surface.commit();
+
+            // TODO save and reuse buffer when the window size is unchanged.  This is especially
+            // useful if you do damage tracking, since you don't need to redraw the undamaged parts
+            // of the canvas.
+        }
+    }
+}
+
+delegate_compositor!(CosmicBg);
+delegate_output!(CosmicBg);
+delegate_shm!(CosmicBg);
+
+delegate_layer!(CosmicBg);
+
+delegate_registry!(CosmicBg);
+
+impl ProvidesRegistryState for CosmicBg {
+    fn registry(&mut self) -> &mut RegistryState {
+        &mut self.registry_state
+    }
+    registry_handlers![CompositorState, OutputState, ShmState, LayerState,];
+}
